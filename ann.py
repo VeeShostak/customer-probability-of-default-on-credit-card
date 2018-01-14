377072fa6372325b0440a6ee535289e5fe6a9206
VeeShostak <VeeShostak@users.noreply.github.com>
2018-01-13 22:16:44 -0500

    apply Feature Scaling

--- ann.py
+++ ann.py
@@ -69,3 +69,25 @@ y = dataset.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+
+# In[5]:
+# Feature Scaling to standardize the range of independent variables or features of data (normalize).
+# we want to have standard normally distributed data and dont want to have one independent var dominating another one.
+# Also due to intensive calculations and parallel execution, scaling eases these calculations.
+
+# If one of the features has a broad range of values, the distance will be governed by
+# this particular feature. Therefore, the range of all features should be normalized so 
+# that each feature contributes approximately proportionately to the final distance.
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+'''
+    fit_transform vs transform: https://datascience.stackexchange.com/questions/12321/difference-between-fit-and-fit-transform-in-scikit-learn-models
+(you have to use the same two parameters μ and σ (values) that you used for centering the training set)
+sklearn's transform's fit() just calculates the parameters (e.g. μ and σ)
+and saves them as an internal objects state. 
+Afterwards, you can call its transform() method to apply the transformation to a particular set of examples.
+'''
+X_train = sc.fit_transform(X_train) # Fit to data, then transform it.
+X_test = sc.transform(X_test) # Perform standardization by centering and scaling
+
